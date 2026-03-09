3a529f716faa9154767e0c4b4519993f80e23606
tokytoky <toky.sigogneau@fms-ea.com>
2026-03-09 14:25:09 +0100

    added register to admin.py files to manipulate the models on the Admin pannel

--- django-eval/ezservice/admin.py
+++ django-eval/ezservice/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Service
+from .models import Reservation
+
+
+admin.site.register(Service)
+admin.site.register(Reservation)
