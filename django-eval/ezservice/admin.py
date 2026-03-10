374c39b77cdb2988e4d15cbecc3eb89f80e28b4c
tokytoky <toky.sigogneau@fms-ea.com>
2026-03-10 15:28:56 +0100

    Improved admin display of Service table on the Admin panel

--- django-eval/ezservice/admin.py
+++ django-eval/ezservice/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from .models import Service, Reservation, Skill, UserSkill
 
-admin.site.register(Service)
+class ServiceAdmin (admin.ModelAdmin):
+    list_display = ["service_owner", "service_skill", "is_available"]
+
+admin.site.register(Service,ServiceAdmin)
 admin.site.register(Reservation)
 admin.site.register(Skill)
 admin.site.register(UserSkill)
