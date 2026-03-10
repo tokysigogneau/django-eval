3e665b283e15336e2eb8a7322ff1ce81f212f959
tokytoky <toky.sigogneau@fms-ea.com>
2026-03-10 08:51:28 +0100

    Fixed long bug : restarted the DB creation from the start and edited models.py

--- django-eval/ezservice/models.py
+++ django-eval/ezservice/models.py
@@ -1,15 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
-class Reservation (models.Model):
-    #related name is necessary or else it will not work, the FK need to be different
-    service_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_reservation')
-    service_client = models.ForeignKey(User, default='No one',on_delete=models.CASCADE, related_name='client_reservation')
-
-
 class Service(models.Model):
     service_owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     date = models.DateTimeField ("service_date")
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation (models.Model):
+    reservation_title = models.CharField(max_length=200)
+    #related name is necessary or else it will not work, the FK need to be different
+    service_owner = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='owner_reservation')
+    service_client = models.ForeignKey(User, default='1',on_delete=models.CASCADE, related_name='client_reservation')
+
+    def __str__(self):
+        return self.reservation_title
