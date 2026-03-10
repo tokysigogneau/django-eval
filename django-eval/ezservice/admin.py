b6aa7e4891ea45ac1393561852c2139022e52bda
tokytoky <toky.sigogneau@fms-ea.com>
2026-03-10 13:27:28 +0100

    Restructured the database and models.py again because i forgot something important : the list of skills

--- django-eval/ezservice/admin.py
+++ django-eval/ezservice/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Service
-from .models import Reservation
-
+from .models import Service, Reservation, Skill, UserSkill
 
 admin.site.register(Service)
 admin.site.register(Reservation)
+admin.site.register(Skill)
+admin.site.register(UserSkill)
--- django-eval/ezservice/models.py
+++ django-eval/ezservice/models.py
@@ -1,11 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class Skill (models.Model):
+    skill_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.skill_name
+
+class UserSkill(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
 class Service(models.Model):
     service_owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    service_skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     date = models.DateTimeField ("service_date")
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
