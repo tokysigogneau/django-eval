034a06641bad115867d2bebe23d636afb972d04b
tokytoky <toky.sigogneau@fms-ea.com>
2026-03-10 10:33:43 +0100

    added app_name to urls.py

--- django-eval/ezservice/urls.py
+++ django-eval/ezservice/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+app_name = "ezservice"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
